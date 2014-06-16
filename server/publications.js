ece27ed33057020f6a69d856c5e4c1df681dc88f
Brad Cerasani <bradcerasani@gmail.com>
2014-06-15 23:25:15 -0500

    use single post subscription to ensure user always sees correct post

--- server/publications.js
+++ server/publications.js
@@ -2,6 +2,10 @@ Meteor.publish('posts', function() {
   return Posts.find({}, options);
 });
 
+Meteor.publish('singlePost', function(id) {
+  return id && Posts.find(id);
+});
+
 Meteor.publish('comments', function() {
   return Comments.find({postId: postId});
 });
